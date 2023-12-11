5ad6f2b56590b24aad6c0e2c29fddc6b2c30d8bc
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2023-12-10 17:28:16 -0800

    last commit

--- collatz.py
+++ collatz.py
@@ -3,11 +3,14 @@
 # so its a easy way of implementing collatz conjucture in python ...
 
 n = int(input("Enter the number: "))
-while n < 0 :
+if n == 1 :
+    print("1 is not really a good input,think about it!")
+while  n <= 1 :
   n = int(input("Enter the number: "))
-
-print('1, Number:1')
+  
+print('Number:1=> 1,')
 for i in range(2,n) :
+  print("Number:"+str(i)+"=>", end= " ")
   j = i
   while j > 1 :
     if j % 2 == 0 :
@@ -16,4 +19,4 @@ for i in range(2,n) :
       j = 3*j+1
     print(int(j), end=",")  
     # Dear TA , im not sure if i can use end , but i think its the only way to make the output look clean!
-  print(" Number:"+str(i))
+  print()
